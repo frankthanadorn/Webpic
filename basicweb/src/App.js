94b41221f5a4a846d64c47760b52b97ac2adcd27
frankthanadorn <72251422+frankthanadorn@users.noreply.github.com>
2021-03-17 13:59:03 +0700

    map array

--- basicweb/src/App.js
+++ basicweb/src/App.js
@@ -2,17 +2,38 @@ import AppHeader from './components/AppHeader';
 import './App.css';
 import TattooItem from './components/TattooItem';
 
+const tattoos =[ {
+    title: "แขนเท่ๆ",
+    thumbnailUrl :"/images/tattoo-01-small.jpg"
+                
+},
+{
+    title: "มือเท่ๆ",
+    thumbnailUrl :"/images/tattoo-02-small.jpg"
+                
+}
+,
+{
+    title: "คอเท่ๆ",
+    thumbnailUrl :"/images/tattoo-03-small.jpg"
+                
+},
+{
+    title: "หลังเท่ๆ",
+    thumbnailUrl :"/images/tattoo-04-small.jpg"
+                
+}
+];
 function App() {
-   
+   const tatooElements = tattoos.map((tattoo,index) =>{
+       return <TattooItem key={index} tattoo={tattoo}/>;
+   })
+
     return (
         <div className="App">
           <AppHeader/>
           <div className ="app-grid">
-                <TattooItem title="แขนเท่ๆ" thumbnailUrl ="/images/tattoo-01-small.jpg"/>
-                <TattooItem title="มือเท่ๆ" thumbnailUrl ="/images/tattoo-02-small.jpg"/>
-                <TattooItem title="คอเท่ๆ" thumbnailUrl ="/images/tattoo-03-small.jpg"/>
-                <TattooItem title="หลังเท่ๆ" thumbnailUrl ="/images/tattoo-04-small.jpg"/>
-                
+                {tatooElements}
                 
           </div>
           
--- basicweb/src/components/TattooItem.js
+++ basicweb/src/components/TattooItem.js
@@ -1,10 +1,10 @@
 import './TattooItem.css';
 function TattooItem(props) {
-    const {title,thumbnailUrl} = props;
+    const {tattoo} = props;
     return (
         <div className="tattoo-item">
-            <img src={thumbnailUrl} />
-            <h4>{title}</h4>
+            <img src={tattoo.thumbnailUrl} />
+            <h4>{tattoo.title}</h4>
         </div>
 
     );
