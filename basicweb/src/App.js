870802a5572849bd6734c23ac16b93926209e51a
frankthanadorn <72251422+frankthanadorn@users.noreply.github.com>
2021-03-17 12:29:11 +0700

    add component
    tatooitem.js

--- basicweb/src/App.js
+++ basicweb/src/App.js
@@ -1,28 +1,17 @@
 import AppHeader from './AppHeader';
 import './App.css';
-
+import TattooItem from './Tattooitem';
 function App() {
    
     return (
         <div className="App">
           <AppHeader/>
           <div className ="app-grid">
-              <div className="tattoo-item">
-                  <img src="/images/tattoo-01-small.jpg"/>
-                  <h4>แมว</h4>
-                  </div>
-                  <div className="tattoo-item">
-                  <img src="/images/tattoo-02-small.jpg"/>
-                  <h4>แมว</h4>
-                  </div>
-                  <div className="tattoo-item">
-                  <img src="/images/tattoo-03-small.jpg"/>
-                  <h4>แมว</h4>
-                  </div>
-                  <div className="tattoo-item">
-                  <img src="/images/tattoo-04-small.jpg"/>
-                  <h4>แมว</h4>
-                  </div>
+                <TattooItem/>
+                <TattooItem/>
+                <TattooItem/>
+                <TattooItem/>
+                  
           </div>
         </div>
     );
