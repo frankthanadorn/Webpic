781e7fbf786704c25d8b058c207cefae6cef4382
frankthanadorn <72251422+frankthanadorn@users.noreply.github.com>
2021-03-17 14:05:50 +0700

    array component

--- basicweb/src/App.js
+++ basicweb/src/App.js
@@ -1,31 +1,11 @@
 import AppHeader from './components/AppHeader';
 import './App.css';
 import TattooItem from './components/TattooItem';
+import Tattoos from './components/Tattoos';
 
-const tattoos =[ {
-    title: "แขนเท่ๆ",
-    thumbnailUrl :"/images/tattoo-01-small.jpg"
-                
-},
-{
-    title: "มือเท่ๆ",
-    thumbnailUrl :"/images/tattoo-02-small.jpg"
-                
-}
-,
-{
-    title: "คอเท่ๆ",
-    thumbnailUrl :"/images/tattoo-03-small.jpg"
-                
-},
-{
-    title: "หลังเท่ๆ",
-    thumbnailUrl :"/images/tattoo-04-small.jpg"
-                
-}
-];
+
 function App() {
-   const tatooElements = tattoos.map((tattoo,index) =>{
+   const tatooElements = Tattoos.map((tattoo,index) =>{
        return <TattooItem key={index} tattoo={tattoo}/>;
    })
 
