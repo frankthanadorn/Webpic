d22a467b5b085945ffab58c8e494d6a78089a7a9
frankthanadorn <72251422+frankthanadorn@users.noreply.github.com>
2021-03-17 12:17:07 +0700

    create react app

---
+++ basicweb/src/App.js
@@ -0,0 +1,33 @@
+
+import './App.css';
+
+function App() {
+   
+    return (
+        <div className="App">
+          <header className="app-header">
+              <img className="app-header-logo" src="/images/logo.png"/>
+          </header>
+          <div className ="app-grid">
+              <div className="tattoo-item">
+                  <img src="/images/tattoo-01-small.jpg"/>
+                  <h4>แมว</h4>
+                  </div>
+                  <div className="tattoo-item">
+                  <img src="/images/tattoo-02-small.jpg"/>
+                  <h4>แมว</h4>
+                  </div>
+                  <div className="tattoo-item">
+                  <img src="/images/tattoo-03-small.jpg"/>
+                  <h4>แมว</h4>
+                  </div>
+                  <div className="tattoo-item">
+                  <img src="/images/tattoo-04-small.jpg"/>
+                  <h4>แมว</h4>
+                  </div>
+          </div>
+        </div>
+    );
+}
+
+export default App;
